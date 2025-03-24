ab82f31e3746ab27da146065b6d34d54f2a2e369
Jonathan Mazin <jmazin@brainstation.io>
2025-03-24 14:08:41 -0400

    fix table, auth

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,16 +1,4 @@
 const authenticate = (req, res, next) => {
-    const authorizationHeader = req.headers.authorization;
-    
-    if (!authorizationHeader) {
-        return res.status(401).json({ error: 'Authorization header missing' });
-    }
-
-    const tokenParts = authorizationHeader.split(' ');
-    if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
-        return res.status(401).json({ error: 'Invalid authorization header format' });
-    }
-
-    req.accessToken = tokenParts[1]; 
     next();  
 };
 
--- migrations/20250317154352_create_projects_table.js
+++ migrations/20250317154352_create_projects_table.js
@@ -3,23 +3,24 @@
  * @returns { Promise<void> }
  */
 export async function up(knex) {
-  await knex.schema.createTable('portfolios', (table) => {
-    table.increments('id').primary();                       
-    table.integer('user_id').unsigned().notNullable();      
-    table.string('repo_name', 255).notNullable();
-    table.string('project_title', 255).notNullable();
-    table.text('description');
-    table.string('languages', 255);                         
-    table.timestamps(true, true);                           
+  await knex.schema.createTable('projects', (table) => {
+      table.increments('id').primary();                       
+      table.integer('user_id').unsigned().notNullable();      
+      table.string('repo_name', 255).notNullable();
+      table.string('project_title', 255).notNullable();
+      table.text('description');
+      table.string('languages', 255);                         
+      table.timestamps(true, true);                           
 
-    table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+    
+  table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
   });
-}
+  }
 
-/**
+  /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-export async function down(knex) {
-  await knex.schema.dropTableIfExists('portfolios');
-}
+  export async function down(knex) {
+  await knex.schema.dropTableIfExists('projects');
+  }
